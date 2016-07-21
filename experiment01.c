7d1987c1cf713a405c2d84bdf39e6c52ba8b2ba7
AKHEEL K M <akheelkm007@gmail.com>
2016-07-21 05:38:19 -0400

    updated

--- experiment01.c
+++ experiment01.c
@@ -1,5 +1,67 @@
 #include <stdio.h>
 
+
+// For creating a new File
+int createFile() {
+	int fileDescriptor = creat("testy.txt", 0777);
+	printf("%d file descriptor while creating file : \n",fileDescriptor);
+}
+
+int openFile() {
+	int fileDescriptor = open("testy.txt", 2);
+	//0 - Read only
+	//1 - Write only
+	//2 - Read Write mode
+
+	printf("File Descriptor %d \n", fileDescriptor);
+	return fileDescriptor;
+}
+
+
+// Writing to Stream
+void writeToStream(int fileDescriptor) {
+	int noOfBytesWritten = write(fileDescriptor, "Hello", 5);
+	printf("No of bytes Written %d \n", noOfBytesWritten );
+
+}
+
+//Sleep the program
+void sleepThePgm() {
+	int n = 2;
+	printf("sleep for %d seconds \n", n);
+	sleep(n);
+}
+
+
+// Reading from a stream
+void readFromStream(int fileDescriptor) {
+	char ArrayOfChar[100];
+	int returnValForRead = read(fileDescriptor, ArrayOfChar, 5);
+	printf("Return Value for read %d \n", returnValForRead);
+	printf("Value read was : %s \n", ArrayOfChar );
+}
+
+// Forking
+void forking() {
+	int childIDOrZero = fork();
+
+	if(childIDOrZero == 0) {
+		//Logic for the Child process
+		printf("This was by the child process \n");
+	}
+
+	else {
+		//Logic for the parent process
+		printf("This was by the parent process \n");
+		printf("The child process ID is %d \n", childIDOrZero);
+	}
+
+}
+
+
+
+
+
 int main() {
 
 	int processId = getpid();
@@ -21,55 +83,23 @@ int main() {
 	printf("Effective  user ID %d \n", EUserID );
 	printf("Effective Group ID %d \n", EgroupID );
 
-	// For creating a new File
-	// int fileDescriptor = creat("testy.txt", 0777);
-	// printf("%d \n",fileDescriptor);
+	// int fileDescriptor1 = createFile();
+	int fileDescriptor2 = openFile();
+
+	// printf("fileDescriptor1 %d \n", fileDescriptor1 );
+	printf("fileDescriptor2 %d \n", fileDescriptor2 );
+
+	writeToStream(fileDescriptor2);
+	// sleepThePgm();
+
+	readFromStream(fileDescriptor2);
 
 
-	int fileDescriptor = open("testy.txt", 2);
-	//0 - Read only
-	//1 - Write only
-	//2 - Read Write mode
+	forking();
 
-	printf("File Descriptor %d \n", fileDescriptor);
+	// close(fileDescriptor1);
+	close(fileDescriptor2);
 
-
-
-	// Writing to Stream
-	// int noOfBytesWritten = write(fileDescriptor, "Hello", 20);
-	// printf("No of bytes Written %d \n", noOfBytesWritten );
-
-
-	//Sleep the program
-	int n = 2;
-	printf("sleep for %d seconds \n", n);
-	sleep(n);
-
-
-
-
-
-	// Reading from a stream
-	char ArrayOfChar[100];
-	int returnValForRead = read(fileDescriptor, ArrayOfChar, 15);
-	printf("Return Value for read %d \n", returnValForRead);
-	printf("Value read was : %s \n", ArrayOfChar );
-
-	close(fileDescriptor);
-
-	// Forking
-	int childIDOrZero = fork();
-
-	if(childIDOrZero == 0) {
-		//Logic for the Child process
-		printf("This was by the child process \n");
-	}
-
-	else {
-		//Logic for the parent process
-		printf("This was by the parent process \n");
-		printf("The child process ID is %d \n", childIDOrZero);
-	}
 
 	return 0;
 }
