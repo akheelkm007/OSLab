87094e3aef56d9c376f6ee49f4388a9a3358daa2
AKHEEL K M <akheelkm007@gmail.com>
2016-08-23 15:12:34 +0530

    Type of file

---
+++ file-type.c
@@ -0,0 +1,19 @@
+#include "time.h"
+#include "sys/stat.h"
+#include "stdio.h"
+
+void main(){
+    struct stat status;
+    FILE *fp;
+    stat("/home/mec/akheel/file-type.c",&status);
+
+    if (status.st_mode & S_IFDIR)
+         printf("It is directory.\n");
+    if (status.st_mode & S_IFCHR)
+         printf("It is chracter file.");
+    if (status.st_mode & S_IFREG)
+         printf("It is regular file.");
+    if (status.st_mode & S_IFBLK)
+         printf("It is block file.");
+
+}
